1f4a22bcbe76d8ca923512b690a0e16877c98d19
Magnus Vooder <magnusvooder@gmail.com>
2024-05-18 08:22:37 +0300

    win% working

--- header.py
+++ header.py
@@ -272,7 +272,7 @@ class Player:
                     self.folded = 1
                     self.winPerc = 0
                     self.tiePerc = 0
-                    print(f"W {winPerc} T {tiePerc}")
+                    print(f"W {self.winPerc} T {self.tiePerc}")
                 else:
                     self.card1Image = Image.open("Images/" + pokerCards[self.hand[0]] + ".png").convert("RGBA")
                     self.card1ImageGui = ImageTk.PhotoImage(self.card1Image)
